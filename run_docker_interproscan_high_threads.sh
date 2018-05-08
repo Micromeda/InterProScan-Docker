d888517d1c28b0b749ded79a310bd858ff7976e2
Lee Bergstrand <leemacboy@gmail.com>
2018-05-08 16:43:52 -0400

    Added wrapper scripts.

---
+++ run_docker_interproscan_high_threads.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Production scrip includes --rm to remove container when done.
+
+echo "Running InterProScan on $1."
+
+docker run --rm --name interproscan -v $PWD:/run -v $PWD/interproscan.properties:/opt/interproscan/interproscan.properties leebergstrand/interproscan:latest -dp --goterms --pathways --appl "Pfam,PIRSF,PANTHER,SMART,TIGRFAM,CDD" -f tsv -o /run/out.ipr -i /run/$1
